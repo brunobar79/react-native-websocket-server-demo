9a27322379b2d21c858fbe113244b9edf5584d32
Bruno Barbieri <brunobar79@gmail.com>
2019-05-27 02:35:41 -0400

    clean up

--- App.js
+++ App.js
@@ -2,22 +2,11 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
- * @format
- * @flow
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import RNWebsocketServer from 'react-native-websocket-server';
-
-
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 
 export default class App extends Component{
